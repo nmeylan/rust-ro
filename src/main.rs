1e87300a84b91ec9fda44cd8dc03a1e63172ca41
unknown <nmeylan@gmail.com>
2021-08-27 14:34:17 +0200

    Add PacketHandler trait
    to be implemented for Login,Char and Map servers

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,26 @@ use std::str::FromStr;
 use std::thread::{spawn, JoinHandle};
 use std::io::{BufReader, BufWriter, Read, Write};
 use std::time::Duration;
+use crate::server::server::{LoginServer, CharServer, MapServer};
 
 fn main() {
     let login = server::server::Server {
         name: "login".to_string(),
         local_port: 6901,
         target: SocketAddr::new(IpAddr::from(Ipv4Addr::new(127, 0, 0, 1)), 6900),
+        packet_handler: LoginServer
     };
     let char = server::server::Server {
         name: "char".to_string(),
         local_port: 6123,
-        target: SocketAddr::new(IpAddr::from(Ipv4Addr::new(127, 0, 0, 1)), 6121)
+        target: SocketAddr::new(IpAddr::from(Ipv4Addr::new(127, 0, 0, 1)), 6121),
+        packet_handler: CharServer
     };
     let map = server::server::Server {
         name: "map".to_string(),
         local_port: 6124,
-        target: SocketAddr::new(IpAddr::from(Ipv4Addr::new(127, 0, 0, 1)), 6122)
+        target: SocketAddr::new(IpAddr::from(Ipv4Addr::new(127, 0, 0, 1)), 6122),
+        packet_handler: MapServer
     };
     let mut handles: Vec<JoinHandle<()>> = Vec::new();
     &handles.push(login.proxy());
