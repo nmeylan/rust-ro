863fc8916df222cd966fe6ac96998c9407d9fb0d
unknown <nmeylan@gmail.com>
2021-08-29 08:24:08 +0200

    Parse packet name to pretify logs
    e.g:
    login > PACKET_CA_LOGIN (55) [64, 0, 37

---
+++ src/server/packet_db.rs
@@ -0,0 +1,20 @@
+use std::{fs, io};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufRead;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    pub static ref PACKETS_DB: HashMap<String, String> = {
+    let mut packets : HashMap<String, String> = HashMap::new();
+    let file = File::open("C:\\dev\\rust-ragnarok-server\\src\\server\\packet_db").unwrap();
+    let reader = io::BufReader::new(file);
+    for line in reader.lines() {
+        if line.is_ok() {
+            let line_fragments: Vec<String> =line.unwrap().split(",").map(|e| e.to_string()).collect();
+            packets.insert(line_fragments.get(0).unwrap().to_string(), line_fragments.get(1).unwrap().to_string());
+        }
+    }
+    packets
+    };
+}
