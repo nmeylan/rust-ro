5c3373007165464295ea8a1fb0e93e850b124d50
unknown <nmeylan@gmail.com>
2021-08-31 20:53:15 +0200

    Identify account making request to map

--- src/server/map.rs
+++ src/server/map.rs
@@ -21,15 +21,18 @@ impl MapServer {
             target: SocketAddr::new(IpAddr::from(Ipv4Addr::new(127, 0, 0, 1)), 6122),
             packet_handler: MapServer {
                 server_context: server_context.clone()
-            }
+            },
         };
         let server_context_ref = server_context.clone();
         thread::Builder::new().name("map server tick".to_string()).spawn(move || {
-            while(true) {
+            while (true) {
                 let server_context_guard = server_context_ref.lock().unwrap();
                 for session in server_context_guard.sessions.values() {
+                    if (session.map_server_socket.as_ref().is_none()) {
+                        continue;
+                    }
                     let mut tcp_stream_guard = session.map_server_socket.as_ref().unwrap().lock().unwrap();
-                    let buffer : [u8; 25] = [0x8D, 0x00, 0x19, 0x00, 0x80, 0x84, 0x1E, 0x00, 0x77, 0x61, 0x6C, 0x6B, 0x69, 0x72, 0x79, 0x20, 0x3A, 0x20, 0x71, 0x77, 0x65, 0x72, 0x74, 0x7A, 0x00];
+                    let buffer: [u8; 25] = [0x8D, 0x00, 0x19, 0x00, 0x80, 0x84, 0x1E, 0x00, 0x77, 0x61, 0x6C, 0x6B, 0x69, 0x72, 0x79, 0x20, 0x3A, 0x20, 0x71, 0x77, 0x65, 0x72, 0x74, 0x7A, 0x00];
                     println!("Send {:02X?} to {}", buffer, session.account_id);
                     tcp_stream_guard.write(&buffer);
                     tcp_stream_guard.flush();
@@ -49,16 +52,27 @@ impl PacketHandler for MapServer {
             let mut rdr = Cursor::new(&packet[2..6]);
             let account_id = rdr.read_u32::<LittleEndian>().unwrap();
 
-            println!("New connection in map server: account {}", account_id);
+            println!("New connection in map server: account {} {}", account_id, tcp_stream.lock().unwrap().peer_addr().unwrap());
             let mut server_context_guard = self.server_context.lock().unwrap();
             let existing_session = server_context_guard.sessions.get(&account_id).unwrap();
             let char_server_socket_ref = existing_session.char_server_socket.as_ref().unwrap().clone();
             server_context_guard.sessions.insert(account_id, Session {
                 char_server_socket: Some(char_server_socket_ref),
                 map_server_socket: Some(tcp_stream),
-                account_id
+                account_id,
             });
+        } else {
+            let mut server_context_guard = self.server_context.lock().unwrap();
+            let incoming_stream_guard = tcp_stream.lock().unwrap();
+            let session = server_context_guard.sessions.iter().find(|(key, value)| {
+                let map_server_socket = value.map_server_socket.as_ref().unwrap().lock().unwrap();
+                map_server_socket.peer_addr().unwrap() == incoming_stream_guard.peer_addr().unwrap()
+            });
+            if (session.is_some()) {
+                println!("found session {}", session.unwrap().0);
+            }
         }
+
         Result::Ok("res".to_string())
     }
 }
