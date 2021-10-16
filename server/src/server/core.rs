76bc9c553cad27042aaacd945f8c4fc53807583a
unknown <nmeylan@gmail.com>
2021-10-16 09:12:23 +0200

    Clean up load unit in player fov

--- server/src/server/core.rs
+++ server/src/server/core.rs
@@ -1,8 +1,9 @@
 // This is the core of this server. As all feature won't be implemented in one shot, the idea is to proxy unimplemented feature to hercules server.
 // For the moment I won't implement TCPListener in this file, but in the "future" proxies will be removed and only this file will have a TCPListener.
 
-use crate::packets::packets::{Packet, PacketUnknown, PacketCaLogin, PacketChEnter, PacketChMakeChar2, PacketChDeleteChar4Reserved, PacketCzEnter2, PacketChSelectChar, PacketCzRestart,  PacketCzReqDisconnect2, PacketCzRequestMove2, PacketCzNotifyActorinit, PacketCzBlockingPlayCancel, PacketZcLoadConfirm};
-use std::sync::{Arc, Mutex, RwLock, RwLockWriteGuard};
+use std::cmp;
+use crate::packets::packets::{Packet, PacketUnknown, PacketCaLogin, PacketChEnter, PacketChMakeChar2, PacketChDeleteChar4Reserved, PacketCzEnter2, PacketChSelectChar, PacketCzRestart, PacketCzReqDisconnect2, PacketCzRequestMove2, PacketCzNotifyActorinit, PacketCzBlockingPlayCancel, PacketZcLoadConfirm, PacketZcNotifyStandentry6, PacketZcNotifyVanish};
+use std::sync::{Arc, Mutex, RwLock, RwLockReadGuard, RwLockWriteGuard};
 use std::thread::{spawn, JoinHandle};
 use crate::repository::lib::Repository;
 use sqlx::{MySql};
@@ -21,9 +22,10 @@ use rand::{Rng};
 use crate::packets::packets_parser::parse;
 use crate::{read_lock, socket_send, write_lock};
 use crate::util::coordinate;
+use crate::util::string::StringUtil;
 
 // Todo make this configurable
-pub const PLAYER_FOV: usize = 14;
+pub const PLAYER_FOV: u16 = 14;
 
 pub struct Server {
     pub sessions: Arc<RwLock<HashMap<u32, Arc<RwLock<Session>>>>>,
@@ -121,13 +123,55 @@ impl CharacterSession {
     pub fn set_movement_task_id(&mut self, id: u128) {
         self.movement_task_id = Some(id);
     }
+
     pub fn get_map_item_at(&self, x: u16, y: u16) -> Option<&Arc<MapItem>> {
-        let i = coordinate::get_cell_index_of(x, y, PLAYER_FOV as u16);
-        self.map_view.get(&i)
+        coordinate::get_item_at(x, y, PLAYER_FOV , &self.map_view)
     }
     pub fn set_map_item_at(&mut self, x: u16, y: u16, item: Arc<dyn MapItem>) {
-        let i = coordinate::get_cell_index_of(x, y, PLAYER_FOV as u16);
+        let i = coordinate::get_cell_index_of(x, y, PLAYER_FOV);
         self.map_view.insert(i, item);
+    }
+
+    pub fn load_units_in_fov(&mut self, map: &Map, session_guard: &RwLockReadGuard<Session>) {
+        let old_map_view = self.map_view.clone();
+        self.map_view.clear();
+        let start_x = cmp::max(self.current_position.x - PLAYER_FOV, 0);
+        let end_x = cmp::min(self.current_position.x + PLAYER_FOV, map.x_size);
+        let start_y = cmp::max(self.current_position.y - PLAYER_FOV, 0);
+        let end_y = cmp::min(self.current_position.y + PLAYER_FOV, map.y_size);
+        for x in start_x..end_x {
+            for y in start_y..end_y {
+                if map.is_warp_cell(x, y) {
+                    let warp = map.get_warp_at(x, y).unwrap();
+                    if coordinate::get_item_at(warp.x, warp.y, PLAYER_FOV, &old_map_view).is_none() {
+                        let mut warp_name = [0 as char; 24];
+                        warp.name.fill_char_array(warp_name.as_mut());
+                        let mut packet_zc_notify_standentry = PacketZcNotifyStandentry6::new();
+                        packet_zc_notify_standentry.set_job(warp.client_item_class());
+                        packet_zc_notify_standentry.set_packet_length(108);
+                        packet_zc_notify_standentry.set_objecttype(6);
+                        packet_zc_notify_standentry.set_aid(warp.id());
+                        packet_zc_notify_standentry.set_pos_dir(Position { x: warp.x, y: warp.y, dir: 0 }.to_pos());
+                        packet_zc_notify_standentry.set_name(warp_name);
+                        packet_zc_notify_standentry.fill_raw();
+
+                        let tcp_stream = session_guard.map_server_socket.as_ref().unwrap();
+                        socket_send!(tcp_stream, packet_zc_notify_standentry.raw());
+                    }
+                    self.set_map_item_at(warp.x, warp.y, warp as Arc<dyn MapItem>);
+                }
+            }
+        }
+        let items = old_map_view.keys().map(|k| *k).collect::<Vec<usize>>();
+        for item in items {
+            if !self.map_view.contains_key(&item) {
+                let mut packet_zc_notify_vanish = PacketZcNotifyVanish::new();
+                packet_zc_notify_vanish.set_gid(old_map_view.get(&item).unwrap().id());
+                packet_zc_notify_vanish.fill_raw();
+                let tcp_stream = session_guard.map_server_socket.as_ref().unwrap();
+                socket_send!(tcp_stream, packet_zc_notify_vanish.raw());
+            }
+        }
     }
 }
 
