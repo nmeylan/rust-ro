0d3cf3cb43ac3f38a1d90cf51674a86b377beef8
unknown <nicolas.meylan@mineral.software>
2021-12-10 11:53:38 +0100

    Restore warp display

--- server/src/server/core/character.rs
+++ server/src/server/core/character.rs
@@ -157,25 +157,24 @@ impl Character {
             new_map_view.push(Some(map_item.clone()));
             seen_items_ids.push(map_item.id());
             if !previous_item_ids.contains(&map_item.id()) {
+                let mut name = [0 as char; 24];
+                map_item.name().fill_char_array(name.as_mut());
+                let mut packet_zc_notify_standentry = PacketZcNotifyStandentry6::new();
+                packet_zc_notify_standentry.set_job(map_item.client_item_class());
+                packet_zc_notify_standentry.set_packet_length(108);
+                packet_zc_notify_standentry.set_name(name);
+                packet_zc_notify_standentry.set_pos_dir(Position { x: map_item.x(), y: map_item.y(), dir: 3 }.to_pos());
+                packet_zc_notify_standentry.set_objecttype(map_item.object_type() as u8);
+                packet_zc_notify_standentry.set_aid(map_item.id());
                 if map_item.object_type() == MapItemType::Mob.value() {
                     let mob = cast!(map_item, Mob);
-                    let mut mob_name = [0 as char; 24];
-                    map_item.name().fill_char_array(mob_name.as_mut());
-                    let mut packet_zc_notify_standentry = PacketZcNotifyStandentry6::new();
-                    packet_zc_notify_standentry.set_job(map_item.client_item_class());
-                    packet_zc_notify_standentry.set_packet_length(108);
-                    packet_zc_notify_standentry.set_objecttype(map_item.object_type() as u8);
                     packet_zc_notify_standentry.set_clevel(3);
                     packet_zc_notify_standentry.set_speed(mob.status.speed as i16);
                     packet_zc_notify_standentry.set_hp(mob.status.hp);
                     packet_zc_notify_standentry.set_max_hp(mob.status.max_hp);
-                    packet_zc_notify_standentry.set_aid(map_item.id());
-                    packet_zc_notify_standentry.set_pos_dir(Position { x: map_item.x(), y: map_item.y(), dir: 3 }.to_pos());
-                    packet_zc_notify_standentry.set_name(mob_name);
-                    packet_zc_notify_standentry.fill_raw();
-                    session.send_to_map_socket(packet_zc_notify_standentry.raw());
                 }
-
+                packet_zc_notify_standentry.fill_raw();
+                session.send_to_map_socket(packet_zc_notify_standentry.raw());
             }
         }
 
