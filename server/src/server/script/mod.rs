e62e4840fbae917f1018f9ce3dfc90a532ec39cd
unknown <nicolas.meylan@mineral.software>
2022-08-10 20:45:33 +0200

    Implement char temporary variables

--- server/src/server/script/mod.rs
+++ server/src/server/script/mod.rs
@@ -1,3 +1,50 @@
+use std::env::var;
+use std::mem;
+
 pub mod script;
 mod global_variable_handler;
 mod constant;
+
+#[derive(Clone, Eq, Hash, PartialEq, Debug)]
+pub struct GlobalVariableEntry {
+    pub name: String,
+    pub value: Value,
+    pub scope: String,
+    pub index: Option<usize>,
+}
+
+#[derive(Default)]
+pub struct ScriptGlobalVariableStore {
+    pub variables: Vec<GlobalVariableEntry>
+}
+
+impl ScriptGlobalVariableStore {
+    pub fn push(&mut self, variable: GlobalVariableEntry) {
+        self.remove_global_by_name_and_scope(&variable.name, &variable.scope);
+        self.variables.push(variable);
+    }
+
+    pub fn find_global_by_name_and_scope(&self, name: &String, scope: &String) -> Option<GlobalVariableEntry> {
+        self.variables.iter().find(|entry| &entry.name == name && &entry.scope == scope
+            && mem::discriminant(&entry.index) == mem::discriminant(&None)).cloned()
+    }
+
+    pub fn remove_global_by_name_and_scope(&mut self, name: &String, scope: &String) {
+        let position = self.variables.iter().position(|entry| &entry.name == name && &entry.scope == scope
+            && mem::discriminant(&entry.index) == mem::discriminant(&None));
+        if let Some(position) = position {
+            self.variables.remove(position);
+        }
+    }
+
+    pub fn find_global_array_entries(&self, name: &String, scope: &String) -> Vec<GlobalVariableEntry> {
+        self.variables.iter().filter(|entry| &entry.name == name && &entry.scope == scope
+            && entry.index.is_some()).map(|e| e.clone()).collect::<Vec<GlobalVariableEntry>>()
+    }
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+pub enum Value {
+    String(String),
+    Number(i32),
+}
