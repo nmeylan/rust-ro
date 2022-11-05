fee54ac18032dcefa466a43ecaf29e783422dbb5
Nicolas MEYLAN <nmeylan@gmail.com>
2022-11-05 15:08:02 +0100

    wip: npc interaction

--- server/src/server/handler/action/npc.rs
+++ server/src/server/handler/action/npc.rs
@@ -12,28 +12,28 @@ use crate::server::core::request::Request;
 use crate::server::script::script::PlayerScriptHandler;
 
 pub fn handle_contact_npc(server: Arc<Server>, context: Request) {
-    todo!("handle_contact_npc")
-    // let packet_cz_contact_npc = cast!(context.packet(), PacketCzContactnpc);
-    // let npc_id = packet_cz_contact_npc.naid;
-    // let map_items_guard = read_lock!(server.map_items);
-    // let map_item_found = map_items_guard.get(&npc_id);
-    // if map_item_found.is_none() {
-    //     error!("Can't find map item with id: {}", npc_id);
-    //     return;
-    // }
-    // let map_item = map_item_found.unwrap().clone();
-    // let server_clone = server.clone();
-    // let runtime = Runtime::new().unwrap();
-    // let (tx, rx) = mpsc::channel(1);
-    // let session = context.session();
-    // let socket = context.socket();
-    // session.set_script_handler_channel_sender(tx);
-    // let client_notification_channel = context.client_notification_channel().clone();
-    // thread::Builder::new().name(format!("script-player-{}-thread", session.account_id)).spawn(move || {
-    //     let script: &Script = cast!(map_item, Script);
-    //     Vm::run_main_function(server_clone.vm.clone(), script.class_reference, script.instance_reference,
-    //                           Box::new(&PlayerScriptHandler { client_notification_channel, npc_id, server: server_clone.clone(), player_action_receiver: RwLock::new(rx), runtime, session: session.clone() })).unwrap()
-    // }).unwrap();
+    let packet_cz_contact_npc = cast!(context.packet(), PacketCzContactnpc);
+    let npc_id = packet_cz_contact_npc.naid;
+    let maybe_map_item = server.get_map_item(npc_id);
+    if maybe_map_item.is_none() {
+        error!("Can't find map item with id: {}", npc_id);
+        return;
+    }
+    let map_item = maybe_map_item.unwrap();
+    let server_clone = server.clone();
+    let runtime = Runtime::new().unwrap();
+    let (tx, rx) = mpsc::channel(1);
+    let session = context.session();
+    session.set_script_handler_channel_sender(tx);
+    let client_notification_channel = context.client_notification_channel().clone();
+    let character = server.get_character_from_context_unsafe(&context);
+    let map_name = character.current_map_name().clone();
+    let map_instance = character.current_map_instance();
+    thread::Builder::new().name(format!("script-player-{}-thread", session.account_id)).spawn(move || {
+        let script = server_clone.map_item_script(&map_item, &map_name, map_instance).expect("Expect to retrieve script from map instance");
+        Vm::run_main_function(server_clone.vm.clone(), script.class_reference, script.instance_reference,
+                              Box::new(&PlayerScriptHandler { client_notification_channel, npc_id, server: server_clone.clone(), player_action_receiver: RwLock::new(rx), runtime, session: session.clone() })).unwrap()
+    }).unwrap();
 }
 
 pub fn handle_player_next(context: Request) {
