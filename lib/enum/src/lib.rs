8a478f7e04b1e12636160b7a5d6d14628dbb8ea8
Nicolas MEYLAN <nmeylan@gmail.com>
2022-12-30 20:29:43 +0100

    Add proc macro to create enum from string

--- lib/enum/src/lib.rs
+++ lib/enum/src/lib.rs
@@ -6,7 +6,7 @@ use syn::{parse_macro_input, DeriveInput, Variant};
 use syn::Data::Enum;
 
 #[proc_macro_derive(WithNumberValue, attributes(value))]
-pub fn with_value(input: TokenStream) -> TokenStream {
+pub fn with_number_value(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let enum_name = &input.ident;
 
@@ -14,7 +14,7 @@ pub fn with_value(input: TokenStream) -> TokenStream {
         let mut j: usize = 1;
         let from_value_match_arms = enum_data.variants.iter().enumerate().map(|(_, variant)| {
             let variant_name = variant.ident.clone();
-            let maybe_value = get_value(variant);
+            let maybe_value = get_number_value(variant);
             if let Some(value) = maybe_value {
                 j = value;
             }
@@ -27,7 +27,7 @@ pub fn with_value(input: TokenStream) -> TokenStream {
         let mut j: usize = 1;
         let value_match_arms = enum_data.variants.iter().enumerate().map(|(_, variant)| {
             let variant_name = variant.ident.clone();
-            let maybe_value = get_value(variant);
+            let maybe_value = get_number_value(variant);
             if let Some(value) = maybe_value {
                 j = value;
             }
@@ -54,19 +54,94 @@ pub fn with_value(input: TokenStream) -> TokenStream {
                 }
             }
         }
-
     } else {
         quote! {}
     };
     TokenStream::from(res)
 }
 
-fn get_value(variant: &Variant) -> Option<usize> {
+fn get_number_value(variant: &Variant) -> Option<usize> {
     let maybe_value = variant.attrs.iter().find(|attr| attr.path.is_ident("value")).map(|attr| match attr.parse_meta().unwrap() {
         syn::Meta::NameValue(syn::MetaNameValue {
                                  lit: syn::Lit::Int(s),
                                  ..
                              }) => s.to_string().parse::<usize>().unwrap(),
+        _ => panic!("malformed attribute syntax"),
+    });
+    maybe_value
+}
+
+
+#[proc_macro_derive(WithStringValue, attributes(with_string_value_uppercase, value))]
+pub fn with_string_value(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let enum_name = &input.ident;
+    let mut uppercase = false;
+    input.attrs.iter().for_each(|attr| {
+        if attr.path.is_ident("with_string_value_uppercase") {
+            uppercase = true;
+        }
+    });
+    let res = if let Enum(enum_data) = &input.data {
+        let from_value_match_arms = enum_data.variants.iter().enumerate().map(|(_, variant)| {
+            let variant_name = variant.ident.clone();
+            let maybe_value = get_string_value(variant);
+            let string_value = if let Some(value) = maybe_value {
+                value
+            } else if uppercase {
+                format!("{}", variant_name).to_uppercase()
+            } else {
+                format!("{}", variant_name)
+            };
+            let res = quote! {
+                #string_value => #enum_name::#variant_name,
+            };
+            res
+        });
+        let value_match_arms = enum_data.variants.iter().enumerate().map(|(_, variant)| {
+            let variant_name = variant.ident.clone();
+            let maybe_value = get_string_value(variant);
+            let string_value = if let Some(value) = maybe_value {
+                value
+            } else if uppercase {
+                format!("{}", variant_name).to_uppercase()
+            } else {
+                format!("{}", variant_name)
+            };
+            let res = quote! {
+                #enum_name::#variant_name => #string_value.to_string(),
+            };
+            res
+        });
+        quote! {
+            impl #enum_name {
+                pub fn from_string(value: &str) -> Self {
+                    match value {
+                        #(#from_value_match_arms)*
+                        _ => panic!("Can't create enum #enum_name for value {}", value)
+                    }
+                }
+
+                pub fn to_string(&self) -> String {
+                    match self {
+                        #(#value_match_arms)*
+                        _ => panic!("Value can't be found for enum #enum_name")
+                    }
+                }
+            }
+        }
+    } else {
+        quote! {}
+    };
+    TokenStream::from(res)
+}
+
+fn get_string_value(variant: &Variant) -> Option<String> {
+    let maybe_value = variant.attrs.iter().find(|attr| attr.path.is_ident("str")).map(|attr| match attr.parse_meta().unwrap() {
+        syn::Meta::NameValue(syn::MetaNameValue {
+                                 lit: syn::Lit::Int(s),
+                                 ..
+                             }) => s.to_string(),
         _ => panic!("malformed attribute syntax"),
     });
     maybe_value
