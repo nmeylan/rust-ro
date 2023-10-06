7b9cbcdb805fc0cfae8cd5c4d8c02bf584356123
Nicolas MEYLAN <nicolas@nmeylan.com>
2023-10-06 15:34:59 +0200

    Play with skills: simply return dummy packet to trigger animation

--- server/src/server/request_handler/action/mod.rs
+++ server/src/server/request_handler/action/mod.rs
@@ -1,4 +1,8 @@
+use crate::Server;
+use crate::server::model::request::Request;
+
 pub mod action;
 pub mod npc;
 pub mod item;
 pub mod character;
+pub mod skill;
--- server/src/server/request_handler/mod.rs
+++ server/src/server/request_handler/mod.rs
@@ -1,11 +1,12 @@
 use std::sync::Arc;
 use crate::packets::packets::Packet;
-use packets::packets::{PacketCaLogin, PacketChDeleteChar4Reserved, PacketChEnter, PacketChMakeChar, PacketChMakeChar2, PacketChMakeChar3, PacketChSelectChar, PacketCzAckSelectDealtype, PacketCzBlockingPlayCancel, PacketCzChooseMenu, PacketCzContactnpc, PacketCzEnter2, PacketCzInputEditdlg, PacketCzInputEditdlgstr, PacketCzItemPickup, PacketCzItemThrow, PacketCzNotifyActorinit, PacketCzPcPurchaseItemlist, PacketCzPcSellItemlist, PacketCzPlayerChat, PacketCzReqDisconnect2, PacketCzReqname, PacketCzReqnameall2, PacketCzReqNextScript, PacketCzReqTakeoffEquip, PacketCzRequestAct, PacketCzRequestMove, PacketCzRequestMove2, PacketCzRequestTime, PacketCzReqWearEquip, PacketCzRestart, PacketCzStatusChange, PacketCzUpgradeSkilllevel, PacketCzUseItem, PacketUnknown, PacketZcNotifyTime};
+use packets::packets::{PacketCaLogin, PacketChDeleteChar4Reserved, PacketChEnter, PacketChMakeChar, PacketChMakeChar2, PacketChMakeChar3, PacketChSelectChar, PacketCzAckSelectDealtype, PacketCzBlockingPlayCancel, PacketCzChooseMenu, PacketCzContactnpc, PacketCzEnter2, PacketCzInputEditdlg, PacketCzInputEditdlgstr, PacketCzItemPickup, PacketCzItemThrow, PacketCzNotifyActorinit, PacketCzPcPurchaseItemlist, PacketCzPcSellItemlist, PacketCzPlayerChat, PacketCzReqDisconnect2, PacketCzReqname, PacketCzReqnameall2, PacketCzReqNextScript, PacketCzReqTakeoffEquip, PacketCzRequestAct, PacketCzRequestMove, PacketCzRequestMove2, PacketCzRequestTime, PacketCzReqWearEquip, PacketCzRestart, PacketCzStatusChange, PacketCzUpgradeSkilllevel, PacketCzUseItem, PacketCzUseSkill, PacketUnknown, PacketZcNotifyTime};
 use crate::server::model::request::Request;
 use crate::server::request_handler::action::action::{handle_action, handle_pickup_item};
 use crate::server::request_handler::action::character::{handle_player_skill_allocation, handle_player_status_change};
 use crate::server::request_handler::action::item::{handle_player_drop_item, handle_player_equip_item, handle_player_takeoff_equip_item, handle_player_use_item};
 use crate::server::request_handler::action::npc::{handle_contact_npc, handle_player_choose_menu, handle_player_input_number, handle_player_input_string, handle_player_next, handle_player_purchase_items, handle_player_select_deal_type, handle_player_sell_items};
+use crate::server::request_handler::action::skill::handle_use_skill;
 use crate::server::request_handler::char::{handle_blocking_play_cancel, handle_char_enter, handle_delete_reserved_char, handle_disconnect, handle_enter_game, handle_make_char, handle_restart, handle_select_char};
 use crate::server::request_handler::chat::handle_chat;
 use crate::server::request_handler::login::handle_login;
@@ -199,6 +200,10 @@ pub fn handle(server: Arc<Server>, mut context: Request) {
     if context.packet().as_any().downcast_ref::<PacketCzUpgradeSkilllevel>().is_some() {
         debug!("PacketCzUpgradeSkilllevel");
         return handle_player_skill_allocation(server.as_ref(), context);
+    }
+    if context.packet().as_any().downcast_ref::<PacketCzUseSkill>().is_some() {
+        debug!("PacketCzUseSkill");
+        return handle_use_skill(server.as_ref(), context);
     }
     // End Skills
 
