98ba6577b0834899bde971ca591ade92ab99bebe
Nicolas MEYLAN <nicolas@nmeylan.com>
2023-10-07 14:18:49 +0200

    Fix skill animation and various erorrs

--- server/src/server/request_handler/action/skill.rs
+++ server/src/server/request_handler/action/skill.rs
@@ -4,6 +4,7 @@ use crate::server::model::request::Request;
 
 use packets::packets::{Packet, PacketCzUseSkill, PacketZcNotifySkill2, PacketZcUseskillAck2};
 use crate::server::model::events::client_notification::{AreaNotification, AreaNotificationRangeType};
+use crate::util::tick::get_tick_client;
 
 pub fn handle_use_skill(server: &Server, context: Request) {
     let packet_cz_use_skill = cast!(context.packet(), PacketCzUseSkill);
@@ -12,8 +13,9 @@ pub fn handle_use_skill(server: &Server, context: Request) {
     let mut packet_zc_useskill_ack2 = PacketZcUseskillAck2::new(context.packet_ver());
     packet_zc_useskill_ack2.set_target_id(packet_cz_use_skill.target_id);
     packet_zc_useskill_ack2.set_skid(packet_cz_use_skill.skid);
-    packet_zc_useskill_ack2.set_property(12);
-    packet_zc_useskill_ack2.set_aid(context.session().account_id);
+    packet_zc_useskill_ack2.set_property(12);  // element
+    packet_zc_useskill_ack2.set_delay_time(900); // cast time
+    packet_zc_useskill_ack2.set_aid(context.session().char_id());
     packet_zc_useskill_ack2.fill_raw();
     let character = server.state().get_character(context.session().char_id.unwrap()).unwrap();
     server.client_notification_sender().send(Notification::Area(
@@ -25,10 +27,11 @@ pub fn handle_use_skill(server: &Server, context: Request) {
     packet_zc_notify_skill2.set_attack_mt(305);
     packet_zc_notify_skill2.set_target_id(packet_cz_use_skill.target_id);
     packet_zc_notify_skill2.set_damage(30);
+    packet_zc_notify_skill2.set_start_time(get_tick_client());
     packet_zc_notify_skill2.set_attacked_mt(480);
     packet_zc_notify_skill2.set_level(10);
     packet_zc_notify_skill2.set_count(1);
-    packet_zc_notify_skill2.set_aid(context.session().account_id);
+    packet_zc_notify_skill2.set_aid(context.session().char_id());
     packet_zc_notify_skill2.set_action(6);
     packet_zc_notify_skill2.fill_raw();
 
