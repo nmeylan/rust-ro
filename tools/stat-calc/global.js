cf561b4a1602b8cf955d1777e1adc1ac08c04bea
Nicolas MEYLAN <nicolas@nmeylan.com>
2024-01-23 15:05:58 +0100

    Stats calc: fix NaN

--- tools/stat-calc/global.js
+++ tools/stat-calc/global.js
@@ -1,16 +1,8 @@
 // Global we want to get rid of
-global.hasLeftHand = 0;
 global.n_SpSkill = 0;
 global.n_Ses = 0;
 global.n_Enekyori = 0;
 global.w_AG = [100, 95, 90, 86, 82, 79, 76, 74, 72, 71, 70];
-global.n_SkillSW = 0;
-global.n_Skill3SW = 0;
-global.n_Skill4SW = 0;
-global.n_Skill5SW = 0;
-global.n_Skill6SW = 0;
-global.n_Skill7SW = 0;
-global.MonsterStats = 0;
 global.wBCEDPch = 0;
 global.wLAch = 0;
 global.wCriTyuu = 0;
