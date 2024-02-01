88823d8d1818af8dbb8306e3ffdff620c688448a
Nicolas Meylan <nicolas@nmeylan.com>
2024-02-01 13:57:55 +0100

    Store compilation result in db for item script

--- docker/volumes/init.sh
+++ docker/volumes/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 psql -U postgres < /create_role.sql
-psql -U ragnarok < /pg.sql
+psql -U ragnarok < /db/pg.sql
+psql -U ragnarok < /db/alter_itemdb_add_script_compilation_result_column.sql
