4f004d6932afaf614c023818089ac0dd7bb35906
Nicolas MEYLAN <nicolas@nmeylan.com>
2024-03-24 12:38:19 +0100

    Add proc macro to generate PartialEq impl for enum

--- lib/models/src/lib.rs
+++ lib/models/src/lib.rs
@@ -3,3 +3,25 @@ pub mod item;
 pub mod position;
 pub mod status;
 pub mod enums;
+
+
+#[cfg(test)]
+mod tests {
+    use enum_macro::WithEq;
+
+    #[derive(PartialEq)]
+    enum EElement {
+        Fire,
+        Water
+    }
+
+    #[derive(WithEq)]
+    enum AB {
+        EnableSkill,
+        Str(i8),
+        Atk(i8),
+        SkillIdIncrease(u32, i8),
+        #[value_comparison_offset = 1]
+        ElementPercentage(i8, EElement),
+    }
+}
