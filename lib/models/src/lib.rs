28f766dbc3d113261678cae75f6ffe025b109afb
Nicolas Meylan <nicolas@nmeylan.com>
2024-03-24 15:10:58 +0100

    Add proc_macro to generate BonusType methods

--- lib/models/src/lib.rs
+++ lib/models/src/lib.rs
@@ -4,24 +4,30 @@ pub mod position;
 pub mod status;
 pub mod enums;
 
-
 #[cfg(test)]
 mod tests {
-    use enum_macro::WithEq;
+    use crate::enums::EnumStackable;
+    use enum_macro::{WithStackable, WithEq};
 
-    #[derive(PartialEq)]
+    #[derive(PartialEq, Clone, Copy)]
     enum EElement {
         Fire,
         Water
     }
 
-    #[derive(WithEq)]
+    #[derive(WithEq, WithStackable)]
     enum AB {
         EnableSkill,
         Str(i8),
         Atk(i8),
-        SkillIdIncrease(u32, i8),
+        SkillIdIncrease(u32, i32),
         #[value_comparison_offset = 1]
-        ElementPercentage(i8, EElement),
+        #[value_offset = 0]
+        ElementPercentage(u16, EElement),
+        WeaponElement(EElement),
+    }
+    #[test]
+    fn a() {
+        let ab = AB::EnableSkill;
     }
 }
