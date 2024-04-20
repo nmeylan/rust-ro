2939772d1ddc9b92550ab15cdc3335dfadcb199d
Nicolas Meylan <nicolas@nmeylan.com>
2024-04-20 12:39:10 +0200

    Exclude tests from build, using cfg(test)

--- server/src/tests/mod.rs
+++ server/src/tests/mod.rs
@@ -1,11 +1,22 @@
+#[cfg(test)]
 mod inventory_service_test;
+#[cfg(test)]
 mod status_service_test;
+#[cfg(test)]
 mod battle_service_test;
+#[cfg(test)]
 mod common;
+#[cfg(test)]
 mod map_instance_service_test;
+#[cfg(test)]
 mod server_service_test;
+#[cfg(test)]
 mod character_service_tests;
+#[cfg(test)]
 mod skill_tree_service_test;
+#[cfg(test)]
 mod skill_service_test;
+#[cfg(test)]
 mod map_instance_loop_integration_test;
+#[cfg(test)]
 mod item_service_test;
