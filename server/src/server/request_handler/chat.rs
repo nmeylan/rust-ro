31fcbe22d87a2a8d596c3cda60963ed202ec6a1f
Nicolas MEYLAN <nicolas@nmeylan.com>
2025-02-02 15:25:45 +0100

    Fix replaying chat packet

--- server/src/server/request_handler/chat.rs
+++ server/src/server/request_handler/chat.rs
@@ -7,6 +7,7 @@ pub fn handle_chat(server: &Server, context: Request) {
     let packet_player_chat = cast!(context.packet(), PacketCzPlayerChat);
     let char_id = context.session().char_id();
     let character = server.state().get_character_unsafe(char_id);
+    debug!("Received chat from {}: {:?}", character.name, packet_player_chat.msg);
     if packet_player_chat.msg.starts_with(format!("{} : @", character.name).as_str()) { // TODO make symbol configurable
         handle_atcommand(server, context, packet_player_chat);
     }
